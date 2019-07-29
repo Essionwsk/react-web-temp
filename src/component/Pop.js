2d71fcc99b84421398b0eaec46aade191366c35c
essionwsk <yu65632625>
2019-07-29 10:27:30 +0800

    优化pop功能

--- src/component/Pop.js
+++ src/component/Pop.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import ReactDOM, {render} from 'react-dom'
 import {Icon} from 'antd';
 
+let __popPool__ = [];
+
 class Pop extends Component {
     constructor(props) {
         super(props);
@@ -10,8 +12,8 @@ class Pop extends Component {
         this.$$popDialog = React.createRef();
         this.$$popBody = React.createRef();
 
-
         this.state = {
+            key:null,//此弹窗的标识，可以与其他弹窗的key重复，目前主要用于卸载其他组件生成的pop，相同key的弹窗会同时被删除。
             hide: false,
             showTitle: true,
             title: "弹出框",
@@ -29,6 +31,7 @@ class Pop extends Component {
             cancelClass: "",
             content: "自定义 component",
             closeIcon: "",
+            backgroundColor:"",
             onLoad: () => {
 
             }
@@ -36,11 +39,19 @@ class Pop extends Component {
 
         Object.assign(this.state, this.props.para);
 
+        this.id = this.props.id;
+        this.key = this.state.key;
+
         this.remove = () => {
             let $container = this.$$popDialog.current.parentNode;
             let $body = $container.parentNode;
             ReactDOM.unmountComponentAtNode($container);
-            $body.removeChild($container)
+            $body.removeChild($container);
+            __popPool__.map((n,i)=>{
+                if(n.id === this.id){
+                    __popPool__.splice(i,1);
+                }
+            })
         };
 
     }
@@ -64,7 +75,7 @@ class Pop extends Component {
         let _this = this;
         let remove = true;
         if(typeof _this.props.ok === "function"){
-            _this.props.ok(_this.$$popDialog);
+            remove = _this.props.ok(_this.$$popDialog);
         }
         if (remove === false) {
 
@@ -91,24 +102,38 @@ class Pop extends Component {
         return false
     };
 
+    _bindKeydown = ()=>{
+
+    };
+
     componentWillUnmount() {
 
     }
 
     componentDidMount() {
-        this.state.onLoad(this.$$popDialog.current)
+        this.state.onLoad(this.$$popDialog.current);
+        __popPool__.push(this);
     }
 
     render() {
+        let {
+            backgroundColor,
+            width,
+            height,
+            maxHeight,
+            maxWidth,
+        } = this.state;
+
+        backgroundColor = backgroundColor===""?{}:{backgroundColor:backgroundColor};
+        width = !this._isMobile() ?{width:width}:{};
+        height = height===""?{}:{height:height};
+        maxHeight = maxHeight === ""?{}:{maxHeight:maxHeight};
+        maxWidth = maxWidth ===""?{}:{maxWidth:maxWidth};
+
         let style = Object.assign({
-            height: this.state.height,
             margin: "0 auto",
-            // top:this.state.top,
-            "maxHeight": this.state.maxHeight,
-            "maxWidth": this.state.maxWidth,
-        }, !this._isMobile() ? {
-            width: this.state.width,
-        } : {});
+        },maxWidth,maxHeight,height,width,backgroundColor);
+
         let okStyle = this.state.okClass === "" ? "btn-violet" : this.state.okClass;
         let cancelStyle = this.state.cancelClass === "" ? "btn-opacity" : this.state.cancelClass;
 
@@ -130,8 +155,8 @@ class Pop extends Component {
                 }
                 {
                     isNotText?
-                        <div id="popBody" className="pop-dialog-body" ref={this.$$popBody} >{this.state.content}</div>:
-                        <div id="popBody" className="pop-dialog-body" ref={this.$$popBody} dangerouslySetInnerHTML={{__html:this.state.content}} />
+                        <div style={{backgroundColor:style.backgroundColor}} id="popBody" className="pop-dialog-body" ref={this.$$popBody} >{this.state.content}</div>:
+                        <div style={{backgroundColor:style.backgroundColor}} id="popBody" className="pop-dialog-body" ref={this.$$popBody} dangerouslySetInnerHTML={{__html:this.state.content}} />
                 }
                 {this.state.showFooter ? (<div className="pop-dialog-foot">
                     {this.state.showCancel ? <button className={"_cancel btn " + cancelStyle}
@@ -151,19 +176,28 @@ let createMask = ()=>{
     popMask.className = "pop-mask";
     popMask.setAttribute("data-id",id);
     $body.appendChild(popMask);
-    return popMask
+    return {
+        popMask:popMask,
+        id:id
+    }
 };
 
 export default {
     dialog:(para, ok, cancel)=>{
-        let $popMask = createMask();
-        let react = render(<Pop para={para} ok={ok} cancel={cancel}/>,$popMask);
+        let maskObj = createMask();
+        let react = render(
+            <Pop
+                para={para}
+                ok={ok}
+                cancel={cancel}
+                id={maskObj.id}
+            />,maskObj.popMask);
         return {
-            remove: react.remove
+            remove: react?react.remove:""
         }
     },
     alert: function (msg, callback) {
-        let $popMask = createMask();
+        let maskObj = createMask();
         let para = {
             showTitle: false,
             width: "300px",
@@ -174,13 +208,13 @@ export default {
             showCancel: false,
             content: <div className="alert-msg">{msg}</div>,
         };
-        let react = render(<Pop para={para} ok={callback}/>,$popMask);
+        let react = render(<Pop para={para} ok={callback} id={maskObj.id} />,maskObj.popMask);
         return {
             remove: react.remove
         }
     },
     confirm: function (msg, ok, cancel) {
-        let $popMask = createMask();
+        let maskObj = createMask();
         let para = {
             width: "350px",
             title: "提示",
@@ -188,9 +222,54 @@ export default {
             okText: "确定",
             content: <div className="alert-msg">{msg}</div>,
         };
-        let react = render(<Pop para={para} ok={ok} cancel={cancel}/>,$popMask);
+        let react = render(<Pop para={para} ok={ok} cancel={cancel} id={maskObj.id} />,maskObj.popMask);
         return {
             remove: react.remove
         }
+    },
+    warning:function (msg,callback) {
+        let maskObj = createMask();
+        let para = {
+            title:"提示",
+            width:"300px",
+            height:"",
+            top:"20%",
+            showCancel:false,
+            content:<div className="alert-msg">{msg}</div> ,
+        };
+        let react = render(<Pop para={para} ok={callback} id={maskObj.id} />,maskObj.popMask);
+        return {
+            remove:react.remove,
+            id:maskObj.id
+        }
+    },
+    success:function (msg,callback) {
+        let maskObj = createMask();
+        let para = {
+            title:"提示",
+            width:"350px",
+            height:"",
+            top:"20%",
+            showCancel:false,
+            content:<div className="alert-msg">
+                <Icon type="check-circle" style={{color:"#34CE34",fontSize:"60px",marginBottom:"10px"}}/>
+                <p>{msg}</p>
+            </div>
+        };
+        let react = render(<Pop para={para} ok={callback} id={maskObj.id} />,maskObj.popMask);
+        return {
+            remove:react.remove
+        }
+    },
+    /***
+     * 通过标识 卸载任意单个或多个弹窗
+     * @param key
+     */
+    remove:function (key) {
+        __popPool__.map((n,i)=>{
+            if(n.key === key){
+                n.remove();
+            }
+        })
     }
 };
